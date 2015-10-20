b1f08f5daba5424870a73f379b905299e1971581
Christopher <chris@christopherbow.com>
2015-10-19 20:21:56 -0700

    Worked on the employee database class. Trying to get ArrayList to work.

--- JavaSource/com/corejsf/EmployeeDatabase.java
+++ JavaSource/com/corejsf/EmployeeDatabase.java
@@ -1,6 +1,7 @@
 package com.corejsf;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import javax.inject.Named;
 // or import javax.faces.bean.ManagedBean;
@@ -16,6 +17,22 @@ public class EmployeeDatabase implements Serializable {
        new Employee("Mariko",25,  "Randor"),
        new Employee("John", 26, "Wilson")
    };
+	
+	private ArrayList<Employee> employees2;
+	private int size;
+	
+	public EmployeeDatabase() {
+		employees2 = new ArrayList<Employee>();
+		employees2.add(new Employee("Chris", 1, "cbow"));
+		employees2.add(new Employee("Tony", 2, "tlee"));
+		employees2.add(new Employee("Wesley", 3, "wsura"));
+		employees2.add(new Employee("Anthony", 4, "afigueroa"));
+		size = employees2.size();
+	}
 
    public Employee[] getEmployees() { return employees;}
+   public ArrayList<Employee> getEmployees2() { return employees2;}
+   public int getSize() {
+	   return employees2.size();
+   }
 }
