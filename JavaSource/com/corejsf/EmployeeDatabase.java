3f65f4a1d56a64b8e596123818335c0c29ccb9d5
Tony <zapman89@hotmail.com>
2015-10-23 00:34:09 -0700

    Merge Conflicts should be fine now

--- JavaSource/com/corejsf/EmployeeDatabase.java
+++ JavaSource/com/corejsf/EmployeeDatabase.java
@@ -16,13 +16,6 @@ import javax.enterprise.context.SessionScoped;
 @SessionScoped
 public class EmployeeDatabase implements Serializable {
 	
-	/*private static final Employee[] employees = new Employee[] {
-       new Employee("William",23, "Dupont"),
-       new Employee("Anna", 24, "Keeney"),
-       new Employee("Mariko",25,  "Randor"),
-<<<<<<< HEAD
-       new Employee("John", 26, "Wilson")*/
-	
 	private static final ArrayList<Employee> employees = new ArrayList<Employee>(Arrays.asList(
 		new Employee("William",23, "Dupont"),
 	    new Employee("Anna", 24, "Keeney"),
@@ -45,28 +38,4 @@ public class EmployeeDatabase implements Serializable {
 	      employees.remove(deleteEmp);
 	      return null;
 	   }
-//=======
-//       new Employee("John", 26, "Wilson")
-//   };
-	
-	private ArrayList<Employee> employeesTwo;
-	private int size;
-	
-	public EmployeeDatabase() {
-		employeesTwo = new ArrayList<Employee>();
-		employeesTwo.add(new Employee("Chris", 1, "cbow"));
-		employeesTwo.add(new Employee("Tony", 2, "tlee"));
-		employeesTwo.add(new Employee("Wesley", 3, "wsura"));
-		employeesTwo.add(new Employee("Anthony", 4, "afigueroa"));
-		size = employeesTwo.size();
-	}
-
-   //public Employee[] getEmployees() { return employees;}
-   
-   public ArrayList<Employee> getEmployees2() { return employeesTwo;}
-   
-   public int getSize() {
-	   return employeesTwo.size();
-   }
-//>>>>>>> b1f08f5daba5424870a73f379b905299e1971581
 }
